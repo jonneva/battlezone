92aa293d014a691d55323ddb0ff7c23510c81929
charlesnburns <charlesnburns@ca331332-a939-11dd-a714-1752b6e4c161>
2008-11-30 08:39:36 +0000

    Moved sound and keyboard controlls to controller.h/controller.cpp

--- Window.h
+++ Window.h
@@ -5,12 +5,10 @@
 #include <QMainWindow>
 #include <QApplication>
 #include <QKeyEvent>
-#include <QMap>
 #include <QLayout>
-#include <QSound>
 #include <QLabel>
 #include "controller.h"
-#include "config.h"
+
 
 class controller;
 
@@ -23,20 +21,10 @@ public:
 	Window();
 //	view* fov;
 //	QLabel* test;
-	config myConfig;
 
-	protected:
-
-	virtual void keyPressEvent( QKeyEvent *event );
-	virtual void keyReleaseEvent( QKeyEvent *event );
 
 private:
-	enum Action { Forward, RotateLeft, RotateRight, Reverse, Shoot, Exit  };
-	QMap<int,Action> keys;
 
-	QSound *move;
-	QSound *fire;
-	QSound *kill;
 
 	// TODO: Remove these lines. Just for testing.
 	controller *myController;
