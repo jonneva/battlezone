af0b564d03076fcc96e4947b8813f250ed9478aa
russtrey.88 <russtrey.88@ca331332-a939-11dd-a714-1752b6e4c161>
2008-12-03 18:17:16 +0000

    The model reader is now working. It creates three lists one for each model. Each list is a list of point3d. point3d is a struct that contains 6 points.

--- model.cpp
+++ model.cpp
@@ -1,7 +1,5 @@
 #include "model.h"
 
-
-
 model::model()
 {
     parseModels("models.dat");
@@ -21,13 +19,14 @@ void model::parseModels(QString filePath)
         }
         else
         {
+
         QString currentLine = file.readLine();
         currentLine = file.readLine();
-        while(currentLine.size() != 0)
+        while(currentLine.size() != 1)
         {
             point3d line;
             int i = 0;
-            int e = 0;
+            int e = 1;
 
             while(e <= 6)
             {
@@ -45,19 +44,82 @@ void model::parseModels(QString filePath)
                     e++;
                 }
                 i++;
-                tank.append(line);
             }
+            tank.append(line);
             currentLine = file.readLine();
         }
-        this->print();
+        qDebug() << "Tank points:";
+        this->print(tank);
+
+        currentLine = file.readLine();
+        currentLine = file.readLine();
+        while(currentLine.size() != 1)
+        {
+            point3d line;
+            int i = 0;
+            int e = 1;
+
+            while(e <= 6)
+            {
+                QString point = currentLine.section(' ', i, i);
+                if(point != "")
+                {
+                    float p = point.toFloat();
+                    if( e == 1 ) { line.x1 = p; }
+                    else if ( e == 2 ) {line.y1 = p; }
+                    else if ( e == 3 ) {line.z1 = p; }
+                    else if ( e == 4 ) {line.x2 = p; }
+                    else if ( e == 5 ) {line.y2 = p; }
+                    else if ( e == 6 ) {line.z2 = p; }
+                    else{}
+                    e++;
+                }
+                i++;
+            }
+            deadTank.append(line);
+            currentLine = file.readLine();
+        }
+        qDebug() << "Dead Tank points:";
+        this->print(deadTank);
+
+        currentLine = file.readLine();
+        currentLine = file.readLine();
+        while(currentLine.size() != 1)
+        {
+            point3d line;
+            int i = 0;
+            int e = 1;
+
+            while(e <= 6)
+            {
+                QString point = currentLine.section(' ', i, i);
+                if(point != "")
+                {
+                    float p = point.toFloat();
+                    if( e == 1 ) { line.x1 = p; }
+                    else if ( e == 2 ) {line.y1 = p; }
+                    else if ( e == 3 ) {line.z1 = p; }
+                    else if ( e == 4 ) {line.x2 = p; }
+                    else if ( e == 5 ) {line.y2 = p; }
+                    else if ( e == 6 ) {line.z2 = p; }
+                    else{}
+                    e++;
+                }
+                i++;
+            }
+            projectile.append(line);
+            currentLine = file.readLine();
+        }
+        qDebug() << "Projectile points:";
+        this->print(projectile);
     }
 }
 
-void model::print()
+void model::print(QList<point3d> printMe)
 {
-
-    for(int i = 0; i < tank.size(); i++)
+    for(int i = 0; i < printMe.size(); i++)
     {
-        qDebug() << "x1:" << tank[i].x1 << endl;
+        qDebug() << printMe[i].x1 << "  " << printMe[i].y1 << "  " << printMe[i].z1 << "      "
+                 << printMe[i].x2 << "  " << printMe[i].y2 << "  " << printMe[i].z2 << endl;
     }
 }
